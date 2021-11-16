dcb5d89d4ea8b4228f029b542bfc33e0d092106b
Stefan Sold <ssold@web.de>
2021-11-16 10:19:11 +0100

    put it together now

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+require('dotenv').config();
+import express, {Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -29,11 +30,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+	// Image URL for testing: https://www.hdwallpaper.nu/wp-content/uploads/2015/02/Funny-Cat-Hidden.jpg
+	// filteredimage?image_url=https://www.hdwallpaper.nu/wp-content/uploads/2015/02/Funny-Cat-Hidden.jpg
+	
+	app.get("/filteredimage", ( req: Request, res: Response ) => {
+		// let { image_url }: string = req.query as string;
+		let image_url: string = req.query.image_url as string
+		// If no address is given return this
+		if (!image_url) {
+			// I define this as a bad request
+			return res.status(400).send("Please specify a valid Image URL.");
+		};
+		// An address was given ...
+		filterImageFromURL(image_url).then(
+			function(value) {
+				// Send the file
+				return res.status(200).sendFile(value);
+			})
+			.catch(
+				// The adress is invalid
+				// this image could not be found, so a not found error is raised
+				function(error) { return res.status(404).send("The Image URL is invalid!")}
+			);
+	});
+	
   //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+	app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
